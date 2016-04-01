682cdcd5b78a2ab313df71f3ed2a38226f43c848
Radosław Przebieglec <rprzebieglec@avaya.com>
2016-04-01 13:18:08 +0200

    Fourth Message

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,25 +1,17 @@
 var React = require('react');
 var News = require('./News');
-
-var news = {
-    "_id": "56fbeba95792e2022370fd56",
-    "title": "Wisła Kraków trenuje (prawie) w komplecie. Brakuje tylko Donalda Guerriera",
-    "image": "http://bi.gazeta.pl/im/54/c0/12/z19664468D.jpg",
-    "link": "http://www.krakow.sport.pl/sport-krakow/1,115698,19839562,wisla-krakow-trenuje-prawie-w-komplecie-brakuje-tylko-donalda.html",
-    "source_id": {
-        "_id": "555b72732466592902353a0c",
-        "name": "Sport.PL",
-        "url": "http://www.krakow.sport.pl/sport-krakow/0,115698.html"
-    },
-    "__v": 0,
-    "updated_at": "2016-03-30T15:07:21.916Z"
-};
+var newses = require('json!./../newses.json');
 
 var Main = React.createClass({
     render: function () {
+        var newsesDOM = newses.map(function(news){
+          return (
+            <News news={news}/>
+          );
+        });
         return (
             <div>
-                <News news={news}/>
+                {newsesDOM}
                 {this.props.children}
             </div>
         );
--- app/components/NewsPic.js
+++ app/components/NewsPic.js
@@ -1,7 +1,6 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 
-var styles = {background: '#ABB7B7', height: 280, overflow: 'hidden'};
 //function NewsPic(props) {
 //    return (
 //        <div style={styles}>
@@ -17,6 +16,8 @@ var styles = {background: '#ABB7B7', height: 280, overflow: 'hidden'};
 //    );
 //}
 
+var styles = {background: '#ABB7B7', height: 280, overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center'};
+var imageStyles = {};
 var NewsPic = React.createClass({
     propTypes: {
         imageUrl: PropTypes.string.isRequired,
@@ -31,11 +32,20 @@ var NewsPic = React.createClass({
         var image = new Image();
         image.src = this.props.imageUrl;
         image.onload = function () {
+          var ratio = 2.035714285714286;
+          var ratio2 = image.width / image.height;
+          if(ratio2 > ratio){
+            imageStyles.minHeight = '100%';
+              imageStyles.minWidth = 'auto';
+          }else{
+            imageStyles.minWidth = '100%';
+              imageStyles.minHeight = 'auto';
+          }
             this.setState({imageSrc: image.src});
         }.bind(this);
         return (
             <div style={styles}>
-                <img src={this.state.imageSrc} alt={this.props.title}/>
+                <img src={this.state.imageSrc} alt={this.props.title} style={imageStyles}/>
             </div>
         );
     }
